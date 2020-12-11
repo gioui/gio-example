8a1c43264169ef4331f643fd1768369f82cd97b3
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-12-11 12:01:51 -0500

    gio-extras/colorpicker: add color picker widget example
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

---
+++ gio-extras/colorpicker/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	"gioui.org/widget/material"
+	"git.sr.ht/~whereswaldon/colorpicker"
+)
+
+func main() {
+	go func() {
+		w := app.NewWindow()
+		if err := loop(w); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+type (
+	C = layout.Context
+	D = layout.Dimensions
+)
+
+var white = color.NRGBA{R: 0xff, G: 0xff, B: 0xff, A: 0xff}
+
+func loop(w *app.Window) error {
+	th := material.NewTheme(gofont.Collection())
+	background := white
+	current := color.NRGBA{R: 255, G: 128, B: 75, A: 255}
+	picker := colorpicker.State{}
+	picker.SetColor(current)
+	muxState := colorpicker.NewMuxState(
+		[]colorpicker.MuxOption{
+			{
+				Label: "current",
+				Value: &current,
+			},
+			{
+				Label: "background",
+				Value: &th.Palette.Bg,
+			},
+			{
+				Label: "foreground",
+				Value: &th.Palette.Fg,
+			},
+		}...)
+	background = *muxState.Color()
+	var ops op.Ops
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			log.Println("destroyed")
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			if muxState.Changed() {
+				background = *muxState.Color()
+				log.Printf("mux changed")
+			}
+			if picker.Changed() {
+				current = picker.Color()
+				background = *muxState.Color()
+				log.Printf("picker changed")
+			}
+			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return colorpicker.PickerStyle{Label: "Current", Theme: th, State: &picker}.Layout(gtx)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{}.Layout(gtx,
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							return colorpicker.Mux(th, &muxState, "Display Right:").Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							size := gtx.Constraints.Max
+							paint.FillShape(gtx.Ops, background, clip.Rect(image.Rectangle{Max: size}).Op())
+							return D{Size: size}
+						}),
+					)
+				}),
+			)
+			e.Frame(gtx.Ops)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.13
 
 require (
 	gioui.org v0.0.0-20201211172859-bd7bb4d5d2f3
+	git.sr.ht/~whereswaldon/colorpicker v0.0.0-20201207220634-905cd7cc7248
 	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20191125211704-12ad95a8df72
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200222043503-6f7a984d4dc4
 	github.com/google/go-github/v24 v24.0.1
-	golang.org/x/exp v0.0.0-20191002040644-a1355ae1e2c3
-	golang.org/x/image v0.0.0-20200618115811-c13761719519
+	golang.org/x/exp v0.0.0-20201203231725-fa01524bc59d
+	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 )
