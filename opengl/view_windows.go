75ef8c632b492cf913c870967969a17f0c72d8bf
Chris Waldon <christopher.waldon.dev@gmail.com>
2024-06-27 15:54:00 -0400

    opengl: [Windows] fix build
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- opengl/view_windows.go
+++ opengl/view_windows.go
@@ -15,6 +15,6 @@ import (
 import "C"
 
 func nativeViewFor(e app.ViewEvent, _ image.Point) (C.EGLNativeWindowType, func()) {
-	return C.EGLNativeWindowType(unsafe.Pointer(e.HWND)), func() {}
+	return C.EGLNativeWindowType(unsafe.Pointer(e.(app.Win32ViewEvent).HWND)), func() {}
 }
 func nativeViewResize(e app.ViewEvent, view C.EGLNativeWindowType, newSize image.Point) {}
